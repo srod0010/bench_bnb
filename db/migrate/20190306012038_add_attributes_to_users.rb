834820eb2904af7a95f7a7b39fee068055fa2064
Savio Rodrigues <saviofrod@gmail.com>
2019-03-05 17:43:02 -0800

    Add attributes to user table

---
+++ db/migrate/20190306012038_add_attributes_to_users.rb
@@ -0,0 +1,9 @@
+class AddAttributesToUsers < ActiveRecord::Migration[5.2]
+  def change
+    add_column :users, :username, :string, null: false
+    add_column :users, :password_digest, :string, null: false
+    add_column :users, :session_token, :string, null: false
+    add_index :users, :username, unique: true
+    add_index :users, :session_token, unique: true
+  end
+end
