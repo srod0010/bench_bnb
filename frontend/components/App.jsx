710a96ebc28d2fe8869ae1f8581f91a8a1993800
Savio Rodrigues <saviofrod@gmail.com>
2019-03-07 19:13:00 -0800

    Update

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
 import GreetingContainer from './greeting/greeting_container';
+import LoginFormContainer from './session_form/login_form_container';
+import SignupFormContainer from './session_form/signup_form_container';
 
 const App = () => (
     <div>
@@ -7,7 +17,10 @@ const App = () => (
             <h1>Bench BnB</h1>
             <GreetingContainer />
         </header>
+
+        <Route path="/login" component={LoginFormContainer} />
+        <Route path="/signup" component={SignupFormContainer} />
     </div>
-);
+)
 
 export default App;
