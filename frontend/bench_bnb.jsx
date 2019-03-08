637626165d2cc086bbb097ab7957a9c8e6598ecc
Savio Rodrigues <saviofrod@gmail.com>
2019-03-07 19:13:13 -0800

    Add preloaded state if currentUser

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,11 +1,32 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
-import {login, signup, logout} from './util/session_api_util';
+import configureStore from './store/store';
+import Root from './components/root';
+import React from 'react';
+import {RECEIVE_CURRENT_USER} from './actions/session_actions';
+import {loginUser, logout} from './actions/session_actions';
 
-document.addEventListener("DOMContentLoaded", () => {
-    window.login = login;
-    window.signup = signup;
+document.addEventListener('DOMContentLoaded', () => {
+    let store;
+    if (window.currentUser) {
+        const preloadedState = {
+            entities: {
+                users: { [window.currentUser.id]: window.currentUser }
+            },
+            session: { id: window.currentUser.id }
+        };
+        store = configureStore(preloadedState);
+        delete window.currentUser;
+    } else {
+        store = configureStore();
+    }
+
+    // TESTING START
+    window.getState = store.getState;
+    window.dispatch = store.dispatch;
+    window.loginUser = loginUser;
     window.logout = logout;
+    // TESTING END
+
     const root = document.getElementById('root');
-    ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
-})
+    ReactDOM.render(<Root store={store} />, root);
+});
