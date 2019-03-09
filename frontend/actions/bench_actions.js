65d7dac20feea2632965597c8d83bc42dfce1d33
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 13:24:24 -0800

    fetchBenches action and thunk

---
+++ frontend/actions/bench_actions.js
@@ -0,0 +1,15 @@
+import * as APIUtil from '../util/bench_api_util';
+
+export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
+
+export const receiveBenches = benches => ({
+    type: RECEIVE_BENCHES,
+    benches
+})
+
+export const fetchBenches = () => dispatch => {
+    return(
+        APIUtil.fetchBenches()
+            .then(benches => dispatch(receiveBenches(benches)))
+    )
+}
