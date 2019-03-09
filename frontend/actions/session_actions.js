51d5a87a05afc18cf9d14d3176e701651abb3d6e
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 11:48:26 -0800

    update

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -26,7 +26,7 @@ export const signup = user => dispatch => (
     ))
 );
 
-export const loginUser = user => dispatch => (
+export const login = user => dispatch => (
     APIUtil.login(user).then(user => (
         dispatch(receiveCurrentUser(user))
     ), err => (
