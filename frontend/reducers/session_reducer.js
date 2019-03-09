02442a5decbb3ae6f1a1ffec2e2525de984b9fa6
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 11:50:26 -0800

    update

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -1,20 +1,24 @@
-import {RECEIVE_CURRENT_USER, LOGOUT_CURRENT_USER} from '../actions/session_actions';
+import {
+    RECEIVE_CURRENT_USER,
+    LOGOUT_CURRENT_USER,
+} from '../actions/session_actions';
 
-const _defaultState = Object.freeze({
+const _nullUser = Object.freeze({
     id: null
-})
+});
 
-const sessionReducer = (state = _defaultState, action) => {
-    Object.freeze(state)
-    switch(action.type) {
+const sessionReducer = (state = _nullUser, action) => {
+    Object.freeze(state);
+    switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            let newState = Object.assign({}, state, {id: action.currentUser})
-            return newState;
+            return {
+                id: action.currentUser.id
+            };
         case LOGOUT_CURRENT_USER:
-            return _defaultState;
+            return _nullUser;
         default:
             return state;
     }
-}
+};
 
 export default sessionReducer;
