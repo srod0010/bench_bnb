d8944a14f26ed1ffb4aae8fc6385f7fb4e99b1af
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 12:25:33 -0800

    Create api benches controller

---
+++ app/controllers/api/benches_controller.rb
@@ -0,0 +1,23 @@
+class Api::BenchesController < ApplicationController
+    before_action :require_logged_in, only: [:create]
+
+    def index
+        @benches = Bench.all
+        render :index
+    end
+
+    def create
+        @bench = Bench.new(bench_params)
+
+        if @bench.save
+            render :show
+        else
+            render json: @bench.errors.full_messages, status: 422
+        end
+    end
+
+    private
+    def bench_params
+        params.require(:bench).permit(:description, :lat, :lng)
+    end
+end
