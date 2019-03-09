f485c19aae1ecf42565c00fff862eac5a6e11519
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 12:26:06 -0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Bench.delete_all
+User.delete_all
+
+Bench.create!(
+  description: 'alamo square, many dogs',
+  lat: 37.775769,
+  lng: -122.434960
+)
+
+Bench.create!(
+  description: 'UN plaza, food truck access',
+  lat: 37.779760,
+  lng: -122.413820
+)
+
+Bench.create!(
+  description: 'Ocean Beach, gnarly breh',
+  lat: 37.769996,
+  lng: -122.511281
+)
+
+User.create!(
+  username: 'guest',
+  password: 'password'
+)
