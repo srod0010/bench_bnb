fe5736a119d92879a37202697a34d008c5561e0b
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 18:23:01 -0800

    create markers

--- frontend/util/marker_manager/marker_manager.js
+++ frontend/util/marker_manager/marker_manager.js
@@ -1,11 +1,29 @@
 class MarkerManager {
     constructor(map) {
         this.map = map;
-        this.markers = {}
+        this.markers = {};
     }
 
     updateMarkers(benches) {
-        console.log('time to update')
+        const benchesObj = {};
+        benches.forEach(bench => benchesObj[bench.id] = bench);
+
+        benches
+            .filter(bench => !this.markers[bench.id])
+            .forEach(newBench => this.createMarkerFromBench(newBench))
+
+
+    }
+
+    createMarkerFromBench(bench) {
+        const position = new google.maps.LatLng(bench.lat, bench.lng)
+        const marker = new google.maps.Marker({
+            position,
+            map: this.map,
+            benchId: bench.id
+
+        })
+        this.markers[marker.benchId] = marker;
     }
 }
 
