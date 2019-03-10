a5e3fac2e7413563d94671a8f5fe0b8daf8e810f
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 18:59:48 -0800

    update

--- app/controllers/api/benches_controller.rb
+++ app/controllers/api/benches_controller.rb
@@ -2,7 +2,7 @@ class Api::BenchesController < ApplicationController
     before_action :require_logged_in, only: [:create]
 
     def index
-        @benches = Bench.in_bounds(bounds)
+        @benches = bounds ? Bench.in_bounds(bounds) : Bench.all
         render :index
     end
 
