a12037ebf544ab3296badae4d4e52ac0b687b790
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 17:22:28 -0800

    Create marker manager util

---
+++ frontend/util/marker_manager/marker_manager.js
@@ -0,0 +1,10 @@
+class MarkerManager {
+    constructor(map) {
+        this.map = map;
+        this.markers = {}
+    }
+
+    updateMarkers(benches) {
+        console.log('time to update')
+    }
+}
