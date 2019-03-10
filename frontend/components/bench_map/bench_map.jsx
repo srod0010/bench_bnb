feae405ea4d1c4d08dcc977d32378a414e74663a
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 17:09:31 -0800

    Instantiate map

--- frontend/components/bench_map/bench_map.jsx
+++ frontend/components/bench_map/bench_map.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
 class BenchMap extends React.Component {
-    render() {
-        return(
-            <div id="map-container" ref="map"></div>
-        )
+    componentDidMount() {
+        const mapOptions = {
+            center: { lat: 37.7758, lng: -122.435 }, // this is SF
+            zoom: 13
+        };
+
+        // wrap this.mapNode in a Google Map
+        this.map = new google.maps.Map(this.mapNode, mapOptions);
     }
+    render() {
+        // this ref gives us access to the map dom node
+    return (
+      <div id="map-container" ref={ map => this.mapNode = map }></div>
+    )
+  }
 }
 
 export default BenchMap;
