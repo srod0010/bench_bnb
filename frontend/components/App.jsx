699d741c9a7e78643e8ab449938e6e1279bcec4a
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 17:09:14 -0800

    Add search container

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -12,6 +12,7 @@ import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session_form/login_form_container';
 import SignupFormContainer from './session_form/signup_form_container';
 import BenchIndexContainer from './bench_index_container';
+import SearchContainer from './search/search_container';
 
 const App = () => (
     <div>
@@ -22,7 +23,7 @@ const App = () => (
 
         <AuthRoute exact path="/login" component={LoginFormContainer} />
         <AuthRoute exact path="/signup" component={SignupFormContainer} />
-        <Route exact path="/" component={BenchIndexContainer} />
+        <Route exact path="/" component={SearchContainer} />
     </div>
 )
 
