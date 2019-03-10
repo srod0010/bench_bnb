bdc158810d4f496a8ff7c000a167c024d7729837
Savio Rodrigues <saviofrod@gmail.com>
2019-03-09 17:09:23 -0800

    Create search container

---
+++ frontend/components/search/search_container.jsx
@@ -0,0 +1,20 @@
+import { connect } from 'react-redux';
+import { fetchBenches } from '../../actions/bench_actions';
+import Search from './search';
+
+
+const msp = state => {
+    return ({
+        benches: Object.values(state.entities.benches)
+    })
+}
+
+const mdp = dispatch => {
+    return ({
+        fetchBenches: () => dispatch(fetchBenches())
+    })
+}
+
+export default connect(msp, mdp)(Search);
+
+
