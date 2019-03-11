e656854419f3d6fe5fa9bbd3ef0348c3aa6e06f2
Savio Rodrigues <saviofrod@gmail.com>
2019-03-11 12:09:30 -0700

    update bounds action

---
+++ frontend/reducers/filter_reducer.js
@@ -0,0 +1,14 @@
+import {UPDATE_BOUNDS} from '../actions/filter_actions'
+
+const filterReducer = (state = {}, action) => {
+    Object.freeze(state);
+
+    switch(action.type) {
+        case UPDATE_BOUNDS:
+            return action.bounds;
+        default:
+            return state;
+    }
+}
+
+export default filterReducer;
