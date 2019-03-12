ec9756b6908329fc792a97c97d3a7cb13ac2fbfc
Savio Rodrigues <saviofrod@gmail.com>
2019-03-11 17:05:18 -0700

    handleSubmit

--- frontend/components/bench_form/bench_form.jsx
+++ frontend/components/bench_form/bench_form.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import {withRouter} from 'react-router';
 
 class BenchForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {description: '', seating: 2, lat: props.lat, lng: props.lng}
         this.update = this.update.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    navigateToSearch() {
+        this.props.history.push('/');
     }
 
     update(field) {
@@ -16,15 +22,23 @@ class BenchForm extends React.Component {
         }
     }
 
+    handleSubmit(e) {
+        e.preventDefault();
+        // debugger;
+        this.props.createBench(this.state)
+        this.navigateToSearch()
+    }
+
     render(){
         // debugger;
         return(
             <div className='form-container'>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <input placeholder="description" type="text" value={this.state.description} onChange={this.update('description')}/>
                     <input placeholder="lat" type="text" disabled value={this.state.lat} onChange={this.update('lat')}/>
                     <input placeholder="lng" type="text" disabled value={this.state.lng} onChange={this.update('lng')}/>
                     <input placeholder="seating" type="text" value={this.state.seating} onChange={this.update('seating')}/>
+                    <input type="submit" value="submit"/>
             
                 </form>
             </div>
@@ -32,4 +46,4 @@ class BenchForm extends React.Component {
     }
 }
 
-export default BenchForm;
+export default withRouter(BenchForm);
