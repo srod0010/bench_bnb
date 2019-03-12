db260ce1f7e154939f1ba14fd3ad28843393d462
Savio Rodrigues <saviofrod@gmail.com>
2019-03-11 17:04:51 -0700

    create bench thunk

--- frontend/actions/bench_actions.js
+++ frontend/actions/bench_actions.js
@@ -1,6 +1,7 @@
 import * as APIUtil from '../util/bench_api_util';
 
 export const RECEIVE_BENCHES = "RECEIVE_BENCHES";
+export const RECEIVE_BENCH = "RECEIVE_BENCH";
 
 export const receiveBenches = benches => ({
     type: RECEIVE_BENCHES,
@@ -12,4 +13,14 @@ export const fetchBenches = (filters) => dispatch => {
         APIUtil.fetchBenches(filters)
             .then(benches => dispatch(receiveBenches(benches)))
     )
+}
+
+export const receiveBench = bench => ({
+    type: RECEIVE_BENCH,
+    bench
+})
+
+export const createBench = bench => dispatch => {
+    APIUtil.createBench(bench)
+        .then(bench => dispatch(receiveBench(bench)))
 }
